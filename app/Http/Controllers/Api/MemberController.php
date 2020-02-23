ee0fd0d29a96444ac880ed3c4b7a78e9d62f269a
JackQiao123 <jack.qiao123@gmail.com>
2020-02-23 10:09:35 +0700

    Backend Development

--- app/Http/Controllers/Api/MemberController.php
+++ app/Http/Controllers/Api/MemberController.php
@@ -164,7 +164,8 @@ class MemberController extends Controller
                     $member = Member::where('members.id', $id)
                             ->leftJoin('players', 'members.id', '=', 'players.member_id')
                             ->leftJoin('weights', 'players.weight_id', '=', 'players.weight_id')
-                            ->select('members.*', 'weights.name AS weight_name', 'weights.weight', 'players.dan', 'players.skill', 'players.expired_date')
+                            ->select('members.*', 'weights.id AS weight_id', 'weights.name AS weight_name', 'weights.weight',
+                                     'players.dan', 'players.skill', 'players.expired_date')
                             ->get();
                 }
 
