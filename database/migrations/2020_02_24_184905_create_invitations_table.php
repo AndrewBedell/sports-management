adee22643a4e6bfd04536dfb1076cc0c329fddd6
JackQiao123 <jack.qiao123@gmail.com>
2020-02-25 03:08:13 +0700

    User invitation

---
+++ database/migrations/2020_02_24_184905_create_invitations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->boolean('is_super');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invitations');
+    }
+}
