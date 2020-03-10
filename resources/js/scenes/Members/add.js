5e28676f67d94f379e123a74600c58bb8a2fb804
JackQiao123 <jack.qiao123@gmail.com>
2020-03-10 18:55:38 +0700

    Phone Number Expression

--- resources/js/scenes/Members/add.js
+++ resources/js/scenes/Members/add.js
@@ -318,7 +318,7 @@ class MemberAdd extends Component {
                   gender: Yup.mixed().required('This field is required!'),
                   birthday: Yup.mixed().required('This field is required!'),
                   email: Yup.string().email('Email is not valid!').required('This field is required!'),
-                  mobile_phone: Yup.string().matches(/^([+])|\d+$/, 'Mobile phone is incorrect!').required('This field is required!'),
+                  mobile_phone: Yup.string().matches(/^\+?[0-9]\s?[-]\s|[0-9]$/, 'Mobile phone is incorrect!').required('This field is required!'),
                   addressline1: Yup.string().required('This field is required!'),
                   // country: Yup.mixed().required('This field is required!'),
                   city: Yup.string().required('This field is required!'),
