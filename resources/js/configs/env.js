cf155ce8fbc8a9384ef503a152e779d8e5f41a7e
ruslankharakhashyan <ruslankharakhashyan@outlook.com>
2020-03-11 20:50:38 +0300

    update payment

--- resources/js/configs/env.js
+++ resources/js/configs/env.js
@@ -2,5 +2,6 @@
 export const API_ENDPOINT = `${window.location.origin}/api`;
 export default {
   API_ENDPOINT,
-  STRIPE_KEY: 'pk_test_mbjb8p5QxdkDLxPqaTSjHjso00nGtzj9Hh'
+  STRIPE_KEY: 'pk_test_mbjb8p5QxdkDLxPqaTSjHjso00nGtzj9Hh',
+  STRIPE_SECRET: 'sk_test_six2kl5sKXr4HIXbAO80AhQR00WsAzpW1j'
 };
