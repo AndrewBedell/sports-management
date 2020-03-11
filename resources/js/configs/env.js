ef808e239035743d8f1defb04ed2264e0f04e7bb
ruslankharakhashyan <ruslankharakhashyan@outlook.com>
2020-03-11 18:45:30 +0300

    update stipe_key

--- resources/js/configs/env.js
+++ resources/js/configs/env.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export const API_ENDPOINT = `${window.location.origin}/api`;
 export default {
-  API_ENDPOINT
+  API_ENDPOINT,
+  STRIPE_KEY: 'pk_test_mbjb8p5QxdkDLxPqaTSjHjso00nGtzj9Hh'
 };
